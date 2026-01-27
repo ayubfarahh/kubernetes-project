66d325b8e5439ea7da4fabdfde5611a7c3e2b4e1
Ayub Farah <ayubfarahh@gmail.com>
2026-01-27 20:06:19 +0000

    set up vpc

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -37,10 +37,47 @@ resource "aws_eip" "eip" {
   domain = "vpc"
 }
 
-resource "aws_nat_gateway" "example" {
+resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public["public_1"].id
 
   depends_on = [aws_internet_gateway.gw]
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  for_each = {
+    for k, v in local.subnets : k => v 
+    if v.type == "public" 
+  }
+
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  for_each = {
+    for k, v in local.subnets : k => v
+    if v.type == "private"
+  }
+
+  subnet_id      = aws_subnet.private[each.key].id
+  route_table_id = aws_route_table.private.id
+}
