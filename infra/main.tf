dbd90b0be37c3ac0e93b70f87bd5e446414ba457
Ayub Farah <ayubfarahh@gmail.com>
2026-01-29 01:20:03 +0000

    eks pod identity agent

--- infra/main.tf
+++ infra/main.tf
@@ -3,13 +3,13 @@ module "vpc" {
 }
 
 module "eks" {
-  source = "./modules/eks"
-  vpc_id     = module.vpc.vpc_id
+  source             = "./modules/eks"
+  vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
-  vpc_cidr  = module.vpc.vpc_cidr
+  vpc_cidr           = module.vpc.vpc_cidr
 }
 
 module "pod" {
-  source = "./modules/pod"
+  source           = "./modules/pod"
   eks_cluster_name = module.eks.cluster_name
 }
--- infra/modules/pod/main.tf
+++ infra/modules/pod/main.tf
@@ -4,14 +4,21 @@ module "external_dns_pod_identity" {
   name = "external-dns"
 
   attach_external_dns_policy    = true
-  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/IClearlyMadeThisUp"]
+  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/Z0705516CKV60PQH3XUN"]
 
   associations = {
     this = {
       cluster_name    = var.eks_cluster_name
       namespace       = "external-dns"
       service_account = "external-dns-sa"
+      
     }
   }
 
+}
+
+resource "aws_eks_addon" "pod_identity_agent" {
+  cluster_name                = var.eks_cluster_name
+  addon_name                  = "eks-pod-identity-agent"
+  
 }
