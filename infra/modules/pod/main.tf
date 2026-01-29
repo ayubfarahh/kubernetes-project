b590fa145072dbb65dc8e6b59f1b13350ac64e45
Ayub Farah <ayubfarahh@gmail.com>
2026-01-29 20:58:10 +0000

    implemented cert manager to gen tls cert

--- infra/modules/pod/main.tf
+++ infra/modules/pod/main.tf
@@ -17,6 +17,23 @@ module "external_dns_pod_identity" {
 
 }
 
+module "cert_manager_pod_identity" {
+  source = "terraform-aws-modules/eks-pod-identity/aws"
+
+  name = "cert-manager"
+
+  attach_cert_manager_policy    = true
+  cert_manager_hosted_zone_arns = ["arn:aws:route53:::hostedzone/Z0705516CKV60PQH3XUN"]
+  associations = {
+    this = {
+      cluster_name    = var.eks_cluster_name
+      namespace       = "cert-manager"
+      service_account = "cert-manager"
+      
+    }
+  }
+}
+
 resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name                = var.eks_cluster_name
   addon_name                  = "eks-pod-identity-agent"
