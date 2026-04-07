e6ea84ce62c4c6233225a25fb479c7202ce213c8
Ayub Farah <ayubfarahh@gmail.com>
2026-04-07 21:45:36 +0100

    Setting up prometheus. Applying the right CRDS for it to function

---
+++ infra/crd_clean.sh
@@ -0,0 +1,9 @@
+kubectl delete crd \
+  prometheuses.monitoring.coreos.com \
+  alertmanagers.monitoring.coreos.com \
+  prometheusrules.monitoring.coreos.com \
+  servicemonitors.monitoring.coreos.com \
+  podmonitors.monitoring.coreos.com \
+  probes.monitoring.coreos.com \
+  scrapeconfigs.monitoring.coreos.com \
+  thanosrulers.monitoring.coreos.com
